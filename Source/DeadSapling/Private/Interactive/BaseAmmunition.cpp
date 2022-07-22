2682f01bbc3dd24f65230a66141a431ace0e4d08
weejo <91460362+weejo@users.noreply.github.com>
2022-07-22 11:50:58 +0200

    newAIshizzle

--- Source/DeadSapling/Private/Interactive/BaseAmmunition.cpp
+++ Source/DeadSapling/Private/Interactive/BaseAmmunition.cpp
@@ -3,18 +3,16 @@
 
 #include "BaseAmmunition.h"
 
-UENUM(BlueprintType)
-enum class EAmmunitionType : uint8
-{
-	E_Flower	UMETA(DisplayName = "FLOWER")
-};
-
+#include "AI/Character/AI_Character_Base.h"
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 ABaseAmmunition::ABaseAmmunition()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
 }
 
 // Called when the game starts or when spawned
@@ -29,4 +27,20 @@ void ABaseAmmunition::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
+
+void ABaseAmmunition::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && (OtherActor != this) && OtherComp) 
+	{
+		if (OtherActor->GetClass()->IsChildOf(AAI_Character_Base::StaticClass()) && IsEvil)
+		{
+			UGameplayStatics::ApplyDamage(OtherActor, Damage, GetInstigatorController(), this, UDamageType::StaticClass());
+
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Bullet did some Damage to AI Character"));
+			this->Destroy();
+		}
+	}
+}
+
+
 
--- Source/DeadSapling/Private/Interactive/BaseAmmunition.h
+++ Source/DeadSapling/Private/Interactive/BaseAmmunition.h
@@ -19,7 +19,19 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, Category="Damage System")
+	bool IsEvil = false;
+
+	UPROPERTY(EditDefaultsOnly, Category="Damage System")
+	float Damage = 1.0f;
+
+	UPROPERTY(EditAnywhere, Category="Damage System")
+	UClass* DamageTypeClass = UDamageType::StaticClass();
+	
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
