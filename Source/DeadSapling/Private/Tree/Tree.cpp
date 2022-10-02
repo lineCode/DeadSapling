e04b3051c3dcc268b0046f4411399f6c7b28b6d7
weejo <wendel.jo@hotmail.com>
2022-10-02 21:59:39 +0200

    some minor changes

---
+++ Source/DeadSapling/Private/Tree/Tree.cpp
@@ -0,0 +1,32 @@
+﻿// ©2022 The Icecream Connection
+
+
+#include "Tree.h"
+
+
+// Sets default values
+ATree::ATree()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ATree::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATree::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ATree::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
